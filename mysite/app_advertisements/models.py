e642d3757ec6d0366c75abdc881de520d221d0b5
Ferium <starkmaks7@gmail.com>
2023-07-31 13:05:28 +0700

    Project

--- mysite/app_advertisements/models.py
+++ mysite/app_advertisements/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+class Advertisement(models.Model):
+    title = models.CharField("заголовок", max_length=60)
+    description = models.TextField("описание")
+    price = models.DecimalField("цена", max_digits=10, decimal_places=2)
+    is_auction = models.BooleanField("уместан ли торг", help_text="Отмеетьте, если торг по объявлению уместен.")
+    updated_ad = models.DateTimeField("дата обновления", auto_now=True)
+    created_ad = models.DateTimeField("дата публикации", auto_now_add=True)
 
-# Create your models here.
+    @admin.display(description="дата публикации")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_ad.date() == timezone.now().date():
+            created_time = self.created_ad.time().strftime("%H:%M")
+            return format_html(
+                "<span style='color: green; font-weight: bold'>Сегодня в {}</span>", created_time
+            )
+        return self.created_ad.strftime("%d.%m.%Y - %H:%M")
+    class Meta:
+        db_table = "advertisements"
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, is_auction={self.is_auction})"
